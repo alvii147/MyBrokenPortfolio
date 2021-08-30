558762bd457ecf47016b7580a71ff4f28ae57741
Zahin <alvii147@gmail.com>
2021-08-29 20:57:02 -0400

    updated bio

--- frontend/src/components/About/data.js
+++ frontend/src/components/About/data.js
@@ -3,7 +3,7 @@ const data = {
         Hi, I'm Zahin! I'm a 3rd year student at the University of Waterloo,
         pursuing a degree in Electrical Engineering
         and a specialization in Artificial Intelligence.
-        I'm currently employed as a Full Stack Developer Intern at Nokia.
+        I'm currently seeking 4-month co-op opportunities for Winter 2022.
         I am often engaged in exploring various domains of software development,
         notably web development, data science and computer vision.
         I am experienced in building Python and Django-based backend web applications,
@@ -12,7 +12,6 @@ const data = {
         such as version control, CI-CD and test-driven development.
         Besides coding, I also enjoy playing soccer and chess.
     `,
-    bioTest: 'Hello'
 }
 
 export default data;
