be560d63c64a2f07973a334fe315ba7737567709
Zahin Zaman <alvii147@gmail.com>
2022-03-20 19:36:31 -0400

    updated about bio

--- frontend/src/components/About/data.js
+++ frontend/src/components/About/data.js
@@ -1,15 +1,15 @@
 const data = {
     bio: `
-        Hi, I'm Zahin! I'm a 3rd year student at the University of Waterloo,
-        pursuing a degree in Electrical Engineering
-        and a specialization in Artificial Intelligence.
-        I'm currently seeking 4-month co-op opportunities for Winter 2022.
+        Hi, I'm Zahin! I'm a 4th year student at the University of Waterloo,
+        pursuing a degree in Electrical Engineering and
+        specializing in Artificial Intelligence.
+        I'm currently seeking 4-month co-op opportunities for Fall 2022.
         I am often engaged in exploring various domains of software development,
         notably web development, data science and computer vision.
         I am experienced in building Python and Django-based backend web applications,
         as well as in experimenting with machine learning algorithms and training neural networks.
         I'm also proficient in software development processes,
-        such as version control, CI-CD and test-driven development.
+        such as version control, DevOps and test-driven development.
         Besides coding, I also enjoy playing soccer and chess.
     `,
 }
