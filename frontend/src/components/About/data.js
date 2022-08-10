3756840c8f3c0387621916fe0370a54a059cdf19
Zahin <alvii147@gmail.com>
2022-08-10 09:49:04 -0400

    typo

--- frontend/src/components/About/data.js
+++ frontend/src/components/About/data.js
@@ -3,7 +3,7 @@ const data = {
         Hi, I'm Zahin! I'm a 4th year student at the University of Waterloo,
         pursuing a degree in Electrical Engineering and
         specializing in Artificial Intelligence.
-        I'm currently seeking full time opportunities for Spring 2022.
+        I'm currently seeking full time opportunities for Spring 2023.
         I am often engaged in exploring various domains of software development,
         notably web development, data science and computer vision.
         I am experienced in building Python and Django-based backend web applications,
